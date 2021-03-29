2434b7e051291fb4a3ffc53c92bebbbaf5d08047
Jayablaze2411 <81559027+Jayablaze2411@users.noreply.github.com>
2021-03-29 15:45:30 +0530

    Update AppController.java
    added comment line in 2

--- AppController.java
+++ AppController.java
@@ -1,5 +1,5 @@
 package com.example.demo.controller;
-
+//jayablaze
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
